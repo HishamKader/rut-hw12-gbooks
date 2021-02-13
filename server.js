c713f3cbdfe798491266e45836c1464143121ab9
Hisham Kader <hisham319@hotmail.com>
2021-02-13 14:00:00 -0500

    changed configuration

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+const connectionString = process.env.DB_CONNECTION.replace('<password>', process.env.DB_PASSWORD).replace('<dbname>', process.env.DB_NAME)
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).then(() => console.log('Succses')).catch(err => console.log(err))
+
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
